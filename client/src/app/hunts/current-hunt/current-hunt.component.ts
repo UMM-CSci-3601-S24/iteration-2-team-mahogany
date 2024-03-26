f9af60f40b710a77530d503ff2825b186e42b3cd
Simon <simonhm31@gmail.com>
2024-03-26 14:14:57 -0500

    changed front end and got rid of buttons

--- client/src/app/hunts/current-hunt/current-hunt.component.ts
+++ client/src/app/hunts/current-hunt/current-hunt.component.ts
@@ -14,6 +14,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { HostService } from 'src/app/hosts/host.service';
 import { Hunt } from '../hunt';
 
+
 @Component({
     selector: 'app-current-hunt',
     standalone: true,
@@ -28,6 +29,7 @@ export class CurrentHuntComponent implements OnInit, OnDestroy {
   secondsElapsed: number = 0;
   private ngUnsubscribe = new Subject<void>();
   private timerSubscription: Subscription;
+
 
   constructor(
     private snackBar: MatSnackBar,
