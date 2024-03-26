008f035724f28dd608383078f2da0ddf51eaa7ed
Simon <simonhm31@gmail.com>
2024-03-25 23:13:44 -0500

    timer starts counting on page load

--- client/src/app/hunts/current-hunt/current-hunt.component.ts
+++ client/src/app/hunts/current-hunt/current-hunt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HuntCardComponent } from "../hunt-card.component";
 import { CompleteHunt } from '../completeHunt';
 import { MatCard, MatCardActions, MatCardContent, MatCardTitle } from '@angular/material/card';
@@ -7,12 +7,11 @@ import { MatDivider } from '@angular/material/divider';
 import { AddTaskComponent } from "../addTask/add-task.component";
 import { DeleteTaskDialogComponent } from '../deleteTask/delete-task-dialog.component';
 import { DeleteHuntDialogComponent } from '../deleteHunt/delete-hunt-dialog.component';
-import { Subject, map, switchMap, takeUntil } from 'rxjs';
+import { Subject, Subscription, interval, switchMap, takeUntil } from 'rxjs';
 import { ActivatedRoute, ParamMap, Router, RouterLink } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HostService } from 'src/app/hosts/host.service';
-import { Hunt } from '../hunt';
 
 @Component({
     selector: 'app-current-hunt',
@@ -21,47 +20,59 @@ import { Hunt } from '../hunt';
     styleUrl: './current-hunt.component.scss',
     imports: [HuntCardComponent, MatCard, MatCardTitle, MatIcon, MatDivider, AddTaskComponent, MatCardContent, MatCardActions, RouterLink]
 })
-export class CurrentHuntComponent implements OnInit, OnDestroy{
-  hunt = input.required<Hunt>();
-  simple = input(true);
-
-  confirmDeleteHunt: boolean =false;
+export class CurrentHuntComponent implements OnInit, OnDestroy {
   completeHunt: CompleteHunt;
   error: { help: string, httpResponse: string, message: string };
-
+  secondsElapsed: number = 0;
   private ngUnsubscribe = new Subject<void>();
+  private timerSubscription: Subscription;
 
-  constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private hostService: HostService, private router: Router, public dialog: MatDialog) { }
+  constructor(
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private hostService: HostService,
+    private router: Router,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    // Start the timer
+    this.timerSubscription = interval(1000)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => {
+        this.secondsElapsed++;
+      });
 
+    // Fetch hunt data
     this.route.paramMap.pipe(
-
-      map((paramMap: ParamMap) => paramMap.get('id')),
-
-      switchMap((id: string) => this.hostService.getHuntById(id)),
-
+      switchMap((paramMap: ParamMap) => this.hostService.getHuntById(paramMap.get('id'))),
       takeUntil(this.ngUnsubscribe)
     ).subscribe({
       next: completeHunt => {
         this.completeHunt = completeHunt;
-        return ;
       },
-      error: _err => {
+      error: err => {
         this.error = {
           help: 'There was a problem loading the hunt – try again.',
-          httpResponse: _err.message,
-          message: _err.error?.title,
+          httpResponse: err.message,
+          message: err.error?.title,
         };
       }
-
     });
   }
 
+  ngOnDestroy(): void {
+    // Cleanup subscriptions
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
   deleteHunt(id: string): void {
-    console.log('Deleting hunt with ID:', id);
     this.hostService.deleteHunt(id).subscribe(() => {
-      console.log('Hunt deleted successfully.');
       this.router.navigate(['/hosts']);
     });
   }
@@ -100,8 +111,8 @@ export class CurrentHuntComponent implements OnInit, OnDestroy{
     });
   }
 
-  ngOnDestroy() {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
+  stopCountUp(): void {
+    // Stop the timer
+    this.timerSubscription.unsubscribe();
   }
 }
