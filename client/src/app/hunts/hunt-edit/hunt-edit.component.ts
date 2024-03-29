5fa39192b9c7d26f91a093daf5efc6cabd7f7534
Jadenkre <raine123@d.umn.edu>
2024-03-28 23:53:05 -0500

    Added validators and rerouting for hunt-edit

--- client/src/app/hunts/hunt-edit/hunt-edit.component.ts
+++ client/src/app/hunts/hunt-edit/hunt-edit.component.ts
@@ -1,30 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { HostService } from 'src/app/hosts/host.service';
-import { MatButtonModule } from '@angular/material/button';
-import { MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from '@angular/material/input';
-import { CommonModule } from '@angular/common';
-
-
-
 
 @Component ({
   selector: 'app-hunt-edit',
   templateUrl: './hunt-edit.component.html',
   styleUrls: ['./hunt-edit.component.scss'],
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule, RouterLink, CommonModule]
+  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatButtonModule, FormsModule, MatInputModule, RouterLink]
 })
-
-
-
 export class HuntEditComponent implements OnInit {
-  huntform: FormGroup;
+  huntForm: FormGroup;
+  huntId: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -32,23 +24,68 @@ export class HuntEditComponent implements OnInit {
     private router: Router,
     private fb: FormBuilder // inject FormBuilder
   ) {
-    this.huntform = this.fb.group({
-      name: new FormControl(),
-      description: new FormControl(),
-      est: new FormControl(),
-      hostId: new FormControl(),
-      numberOfTasks: new FormControl()
-
+    this.huntForm = this.fb.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50)
+      ])),
+      description: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)
+      ])),
+      est: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.max(240),
+        Validators.min(5),
+        Validators.pattern('[0-9]+')
+      ])),
+      hostId: new FormControl('', Validators.required),
+      numberOfTasks: new FormControl('', Validators.required)
     });
+  }
+
+  readonly editHuntValidationMessages = {
+    name: [
+      { type: 'required', message: 'Hunt Name is required' },
+      { type: 'minlength', message: 'Hunt Name must be at least 5 characters long' },
+      { type: 'maxlength', message: 'Hunt Name cannot be more than 50 characters long' }
+    ],
+    description: [
+      { type: 'required', message: 'Description is required' },
+      { type: 'minlength', message: 'Name must be at least 5 characters long' },
+      { type: 'maxlength', message: 'Name cannot be more than 100 characters long' }
+    ],
+    est: [
+      { type: 'required', message: 'Estimated Time is required' },
+      { type: 'max', message: 'Estimated Time cannot exceed 240 minutes' },
+      { type: 'min', message: 'Estimated Time has a minimum of 5 minutes' },
+      { type: 'pattern', message: 'Estimated Time must be a number' }
+    ]
+  };
+
+  formControlHasError(controlName: string): boolean {
+    return this.huntForm.get(controlName).invalid &&
+      (this.huntForm.get(controlName).dirty || this.huntForm.get(controlName).touched);
+  }
+
+  getErrorMessage(name: keyof typeof this.editHuntValidationMessages): string {
+    for(const {type, message} of this.editHuntValidationMessages[name]) {
+      if (this.huntForm.get(name).hasError(type)) {
+        return message;
+      }
+    }
+    return 'Unknown error';
   }
 
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.hostService.getHuntById(id).subscribe(completeHunt => {
+    this.huntId = this.route.snapshot.paramMap.get('id');
+    this.hostService.getHuntById(this.huntId).subscribe(completeHunt => {
       console.log(completeHunt); // log the CompleteHunt object
 
-      this.huntform.setValue({
+      this.huntForm.setValue({
         name: completeHunt.hunt.name,
         description: completeHunt.hunt.description,
         est: completeHunt.hunt.est,
@@ -59,12 +96,12 @@ export class HuntEditComponent implements OnInit {
     });
   }
   onSubmit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.hostService.editHunt(id, this.huntform.value).subscribe({
+    this.huntId = this.route.snapshot.paramMap.get('id');
+    this.hostService.editHunt(this.huntId, this.huntForm.value).subscribe({
       next: () => {
         // Handle successful update
         // For example, navigate back to the list of hunts
-        this.router.navigate(['/hosts']);
+        this.router.navigate(['/hunts', this.huntId]);
       },
       error: (err) => {
         // Handle error
