e6c3328ba74d3b06aaff3b2ccd1478e5e90f1873
Jadenkre <raine123@d.umn.edu>
2024-03-28 21:54:39 -0500

    edit hunt full stying + reroute to original hunt page

--- client/src/app/hunts/task-edit/task-edit.component.ts
+++ client/src/app/hunts/task-edit/task-edit.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { HostService } from 'src/app/hosts/host.service';
-import { ReactiveFormsModule } from '@angular/forms'; // Move this import statement to the top of the file
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from '@angular/material/input';
 
 @Component ({
   selector: 'app-task-edit',
   templateUrl: './task-edit.component.html',
-  styleUrls: ['./task-edit.component.scss'], // Remove the comma here
+  styleUrls: ['./task-edit.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatButtonModule] // Remove this line
+  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatButtonModule, FormsModule, MatInputModule, RouterLink]
 })
 export class TaskEditComponent implements OnInit {
   taskForm: FormGroup;
@@ -24,40 +24,50 @@ export class TaskEditComponent implements OnInit {
     private fb: FormBuilder // inject FormBuilder
   ) {
     this.taskForm = this.fb.group({
-      huntId: new FormControl(),
-      name: new FormControl(),
+      huntId: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50)
+      ])),
     });
   }
 
+  readonly editTaskValidationMessages = {
+    name: [
+      { type: 'required', message: 'Task Name is required' },
+      { type: 'minlength', message: 'Name must be at least 5 characters long' },
+      { type: 'maxlength', message: 'Name cannot be more than 100 characters long' }
+    ]
+  };
 
+  formControlHasError(controlName: string): boolean {
+    return this.taskForm.get(controlName).invalid &&
+      (this.taskForm.get(controlName).dirty || this.taskForm.get(controlName).touched);
+  }
 
+  getErrorMessage(name: keyof typeof this.editTaskValidationMessages): string {
+    for(const {type, message} of this.editTaskValidationMessages[name]) {
+      if (this.taskForm.get(name).hasError(type)) {
+        return message;
+      }
+    }
+    return 'Unknown error';
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.hostService.getTaskById(id).subscribe(task => {
       console.log(task); // log the task object
       this.taskForm.patchValue(task);
-
     });
+    
   }
-
-
-//   onSubmit(): void {
-//     if (this.taskForm.valid) {
-//       this.hostService.editTask(this.taskForm.value).subscribe(() => {
-//         this.router.navigate(['/hunts']);
-//       });
-//     }
-//   }
-// }
 
 onSubmit(): void {
   const id = this.route.snapshot.paramMap.get('id');
   this.hostService.editTask(id, this.taskForm.value).subscribe(() => {
-    this.router.navigate(['/hosts']);
+    this.router.navigate(['/hunts', this.taskForm.get('huntId').value]);
   });
 }
 }
-
-
-
